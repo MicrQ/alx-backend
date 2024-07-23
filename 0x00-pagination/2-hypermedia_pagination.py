427930b6c0d1fee429017a180d5b9ef0f030c1c2
MicrQ <mail.micrq@proton.me>
2024-07-23 22:22:01 +0300

    some modification in the get_hyper method

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,12 +39,13 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ returns a dict """
         start, end = index_range(page, page_size)
+        data = self.get_page(page, page_size)
         return {
-            "page_size": page_size,
+            "page_size": len(data),
             "page": page,
-            "data": self.get_page(page, page_size),
+            "data": data,
             "next_page": page + 1 if end < len(self.__dataset) else None,
-            "prev_page": page - 1 if page > 1 else None,
+            "prev_page": page - 1 if start > 0 else None,
             "total_page": math.ceil(len(self.__dataset) / page_size)
         }
 
