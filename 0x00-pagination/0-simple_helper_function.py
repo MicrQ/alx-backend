579306e8a957d8372c7d92b5987b0fe38d16c800
MicrQ <mail.micrq@proton.me>
2024-07-23 21:24:22 +0300

    pagination project started

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """ pagination """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
