f88e5f3475792f0aeb7b7fc6356ca4267e99a69c
AbenetGebre <mail.micrq@proton.me>
2024-09-02 22:52:20 +0300

    a multiple job creator kue implemented

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -9,7 +9,7 @@ const sendNotification = (phoneNumber, message) => {
 };
 
 queue.process('push_notification_code', (job, done) => {
-    const { phoneNumber, message } = job.data;
-    sendNotification(phoneNumber, message);
-    done();
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
 });
